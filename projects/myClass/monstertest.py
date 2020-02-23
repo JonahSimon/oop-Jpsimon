af3541c457cd10c9b86805563a057cf5ea7fd421
JonahSimon <Jman19922@gmail.com>
2020-02-23 11:44:20 -0700

    adding tests

---
+++ projects/myClass/monstertest.py
@@ -0,0 +1,10 @@
+from monster import Monster
+
+class monsterTest:
+    def __init__(self):
+        self._color = "red"
+        self._arms = 2
+        self._legs = 2
+        self._heads = 3
+        self._dead : bool = False
+
