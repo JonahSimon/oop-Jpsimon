c80ad3e8e04f0764435e0fed141561d4967df716
JonahSimon <Jman19922@gmail.com>
2020-02-23 11:45:15 -0700

    added more properties

--- projects/myClass/monster.py
+++ projects/myClass/monster.py
@@ -1,15 +1,16 @@
 from head import Head
 
-
-class monster:
+class Monster:
     NUMBER_OF_ARMS : int = 2
     NUMBER_OF_LEGS : int = 2
     NUMBER_OF_HEADS : int = 1
+    COLOR : str = "unknown"
     HIT_POINTS : int = 50
 
     def __init__(self, arms : int = NUMBER_OF_ARMS, legs : int = NUMBER_OF_LEGS, \
-        heads : int = NUMBER_OF_HEADS, hp : int = HIT_POINTS):
+        heads : int = NUMBER_OF_HEADS, hp : int = HIT_POINTS, color : str = COLOR):
         self._arms = arms
         self._legs = legs
         self._heads = heads
         self._dead : bool = False
+        self._color : str = color
