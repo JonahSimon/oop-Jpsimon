adebcfb62390d14015830bf04b2a78cfa5f468c3
JonahSimon <Jman19922@gmail.com>
2020-03-21 17:12:40 -0600

    Adding in a second class of hero to implement head

---
+++ projects/myClass/hero.py
@@ -0,0 +1,56 @@
+from head import Head
+
+class Hero:
+    NUMBER_OF_ARMS : int = 2
+    NUMBER_OF_LEGS : int = 2
+    NUMBER_OF_HEADS : int = 1
+    COLOR : str = "unknown"
+    HIT_POINTS : int = 50
+    DEAD : bool = False
+
+    def __init__(self, arms : int = NUMBER_OF_ARMS, legs : int = NUMBER_OF_LEGS, \
+        heads : int = NUMBER_OF_HEADS, hp : int = HIT_POINTS, color : str = COLOR, dead : bool = DEAD):
+        self._arms = arms
+        self._legs = legs
+        self._heads = heads
+        self._dead = dead
+        self._health = hp
+        self._power : str = "none"
+        self._color : str = color
+
+    @property
+    def NumArms(self) -> int:
+        return self._arms
+
+    @property
+    def NumLegs(self) -> int:
+        return self._legs
+
+    @property
+    def NumHeads(self) -> int:
+        return self._heads
+
+    @property
+    def HP(self) -> int:
+        return self._health
+
+    @property
+    def Dead(self) -> bool:
+        return self._dead
+
+    @property
+    def Power(self) -> str:
+        return self._power
+
+    @Power.setter
+    def SetPower(self, power : str) -> None:
+        self._power = power
+
+    @HP.setter
+    def hp(self,value : int) -> None:
+        if value <=0:
+            self._dead = True
+        if self._dead: 
+            self._health = 0
+        else:
+            self._health = value
